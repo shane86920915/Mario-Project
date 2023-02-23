e47a2344ecc8ac0c7daef4d303ecb3a89ac16ab7
Shane A Wilkinson <73564809+shane86920915@users.noreply.github.com>
2023-02-23 00:53:29 +0000

    fixed bug

--- mario.js
+++ mario.js
@@ -1,44 +1,35 @@
 
-
 let userInput = prompt("Enter the height: ");
 console.log(printPyramid(userInput));
 
-
-
-// TODO #2
-// Take in user input for the height
-
-
-/*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
 
 function printPyramid(height) {
   console.log("Uh oh... the pyramid is under construction.");
   console.log("Check back soon, our developers are hard at work as we speak!");
   let str = "";
+  let element = document.getElementById("pyramid")
+  let para
+  let node 
+  
   for (let i = 0; i < height; i++) {
+      str=""
     for (let j = 0; j< height-i; j++) {
       str += "."
     }
+      
     for(let k = 0; k<=i+1; k++){
       str += "#";
     }
-    str += "\n";
+    node = document.createTextNode(str)
+    para = document.createElement("p")
+    para.append(node)
+    element.append(para)
   }
-  
-  document.getElementById('blocks').innerHTML = str;
   return str;
 }
 
+   
+    
 
 
 // TODO 1
